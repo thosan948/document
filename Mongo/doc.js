a9d78256290a0e30be2398ef24b44a34f9d78aa3
Nguyễn Toàn <huutoan.neo@gmail.com>
2019-07-27 11:15:49 +0700

    update

--- Mongo/doc.js
+++ Mongo/doc.js
@@ -12,3 +12,141 @@ db.registerService.insert({
 
 // Update in MongoDB
 db.getCollection('service').update({'text.en': 'Housekeeping'}, {$set: {requireTaskerVersion: '2.10.0'}})
+
+"depositIntruction" : [ 
+  {
+      "city" : "Hồ Chí Minh",
+      "address" : "Ngân hàng Techcombank chi nhánh Thắng Lợi",
+      "accountHolder" : "CÔNG TY TNHH BTASKEE",
+      "accountNumber" : "19130355143051",
+      "bankName" : {
+          "vi" : "Ngân hàng Techcombank",
+          "en" : "Techcombank",
+          "ko" : "Techcombank"
+      },
+      "bankDepartment" : {
+          "vi" : "Chi nhánh Thắng Lợi",
+          "en" : "Thắng Lợi Branch",
+          "ko" : "Thắng Lợi 지점"
+      }
+  }, 
+  {
+      "city" : "Hà Nội",
+      "address" : "Toà nhà Hanoi Creative City, tầng 8 , số 1 Lương Yên,Quận Hai Bà Trưng, Hà Nội",
+      "accountHolder" : "CÔNG TY TNHH BTASKEE",
+      "accountNumber" : "19130355143335",
+      "bankName" : {
+          "vi" : "Ngân hàng Techcombank",
+          "en" : "Techcombank",
+          "ko" : "Techcombank"
+      },
+      "bankDepartment" : {
+          "vi" : "Chi nhánh Hoàng Quốc Việt",
+          "en" : "Hoàng Quốc Việt Branch",
+          "ko" : "Hoàng Quốc Việt Branch"
+      }
+  }, 
+  {
+      "city" : "Đà Nẵng",
+      "address" : "Tầng 3 DNES, 31 Trần Phú, Quận Hải Châu, TP Đà Nẵng",
+      "accountHolder" : "CÔNG TY TNHH BTASKEE",
+      "accountNumber" : "19130355143068",
+      "bankName" : {
+          "vi" : "Ngân hàng Techcombank",
+          "en" : "Techcombank",
+          "ko" : "Techcombank"
+      },
+      "bankDepartment" : {
+          "vi" : "Chi nhánh Chợ Hàn",
+          "en" : "Đà Nẵng Branch",
+          "ko" : "Đà Nẵng Branch"
+      }
+  }, 
+  {
+      "city" : "Bình Dương",
+      "address" : "Ngân hàng Techcombank chi nhánh Bình Dương",
+      "accountHolder" : "CÔNG TY TNHH BTASKEE",
+      "accountNumber" : "19130355143076",
+      "bankName" : {
+          "vi" : "Ngân hàng Techcombank",
+          "en" : "Techcombank",
+          "ko" : "Techcombank"
+      },
+      "bankDepartment" : {
+          "vi" : "Chi nhánh Bình Dương",
+          "en" : "Bình Dương Branch",
+          "ko" : "Bình Dương 지점"
+      }
+  }, 
+  {
+      "city" : "Hải Phòng",
+      "address" : "Ngân hàng Techcombank chi nhánh Thắng Lợi",
+      "accountHolder" : "CÔNG TY TNHH BTASKEE",
+      "accountNumber" : "19130355143051",
+      "bankName" : {
+          "vi" : "Ngân hàng Techcombank",
+          "en" : "Techcombank",
+          "ko" : "Techcombank"
+      },
+      "bankDepartment" : {
+          "vi" : "Chi nhánh Thắng Lợi",
+          "en" : "Thắng Lợi Branch",
+          "ko" : "Thắng Lợi 지점"
+      }
+  }, 
+  {
+      "city" : "Lâm Đồng",
+      "address" : "Ngân hàng Techcombank chi nhánh Thắng Lợi",
+      "accountHolder" : "CÔNG TY TNHH BTASKEE",
+      "accountNumber" : "19130355143051",
+      "bankName" : {
+          "vi" : "Ngân hàng Techcombank",
+          "en" : "Techcombank",
+          "ko" : "Techcombank"
+      },
+      "bankDepartment" : {
+          "vi" : "Chi nhánh Thắng Lợi",
+          "en" : "Thắng Lợi Branch",
+          "ko" : "Thắng Lợi 지점"
+      }
+  }, 
+  {
+      "city" : "Khánh Hòa",
+      "address" : "Ngân hàng Techcombank chi nhánh Thắng Lợi",
+      "accountHolder" : "CÔNG TY TNHH BTASKEE",
+      "accountNumber" : "19130355143051",
+      "bankName" : {
+          "vi" : "Ngân hàng Techcombank",
+          "en" : "Techcombank",
+          "ko" : "Techcombank"
+      },
+      "bankDepartment" : {
+          "vi" : "Chi nhánh Thắng Lợi",
+          "en" : "Thắng Lợi Branch",
+          "ko" : "Thắng Lợi 지점"
+      }
+  }
+],
+
+db.getCollection('taskerSettings').insert( 
+  {
+      _id : "EcwKcv8FhpZNHT39",
+      trainingInput : {
+          link : "https://raw.githubusercontent.com/bTaskee/TrainingV3/master/chapter_1/quizz_8.json",
+          minRateScore : 100,
+          version : 2,
+          numberTest : 2
+      },
+      trainingTasker : {
+          link : "https://raw.githubusercontent.com/bTaskee/TrainingV3/master/courses.json",
+          minRateScore : 80,
+          version : 2,
+          numberTest : 2
+      },
+      handbook : {
+          vi : "http://68.183.179.141:3030/pdf-file/So_tay_bTasker.pdf",
+          ko : "http://68.183.179.141:3030/pdf-file/So_tay_bTasker.pdf",
+          en : "http://68.183.179.141:3030/pdf-file/So_tay_bTasker.pdf"
+      }
+  }
+  )
